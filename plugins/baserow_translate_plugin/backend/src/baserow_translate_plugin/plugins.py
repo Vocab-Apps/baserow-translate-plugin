e2d27f082cd598db7fb7c88c078f1273b1fa69d1
Luc W <lucw@airpost.net>
2023-08-09 12:00:26 +0800

    remove urls, this plugin doesn't introduce an api

--- plugins/baserow_translate_plugin/backend/src/baserow_translate_plugin/plugins.py
+++ plugins/baserow_translate_plugin/backend/src/baserow_translate_plugin/plugins.py
@@ -2,16 +2,6 @@ from loguru import logger
 from baserow.core.registries import Plugin
 from django.urls import path, include
 
-from .api import urls as api_urls
-
 
 class BaserowTranslatePlugin(Plugin):
     type = "baserow_translate_plugin"
-
-    def get_api_urls(self):
-        return [
-            path(
-                "baserow_translate_plugin/",
-                include(api_urls, namespace=self.type),
-            ),
-        ]
